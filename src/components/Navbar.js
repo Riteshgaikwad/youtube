7679d570e6f8c65fe84130a752348973db19b729
Riteshgaikwad <gaikwadritesh2003@gmail.com>
2024-07-04 10:13:26 +0530

    basic done

---
+++ src/components/Navbar.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { GiHamburgerMenu } from 'react-icons/gi'
+
+const Navbar = () => {
+  return (
+    <div>
+        <div>
+            <div>
+                <GiHamburgerMenu/>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
